a55957a5084132a6847e27a70b3250b9e3143db4
Shamim Imtiaz <km_si@ymail.com>
2021-02-27 20:34:30 -0500

    connect sql server, add amployee and role table

--- schema.sql
+++ schema.sql
@@ -7,3 +7,22 @@ CREATE TABLE department (
     name VARCHAR(30) NOT NULL, 
 );
 
+CREATE TABLE role (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL, 
+    salary DECIMAL(6,2) NOT NULL, 
+    department_id INT UNSIGNED NOT NULL, 
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    first_name VARCHAR(30) NOT NULL, 
+    last_name VARCHAR(30) NOT NULL, 
+    role_id INT UNSIGNED NOT NULL, 
+    manager_id INT UNSIGNED,
+    
+CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+
+);
--- seeds.sql
+++ seeds.sql
@@ -5,3 +5,14 @@ INSERT INTO department (name) VALUES ("Finance");
 INSERT INTO department (name) VALUES ("Information Technology");
 INSERT INTO department (name) VALUES ("HR and Recruiting");
 
+INSERT INTO role (title, salary, department_id) VALUES ("Engineer", 70, 3);
+INSERT INTO role (title, salary, department_id) VALUES ("HR Associate", 50, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Finance Manager", 50, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("IT Director", 90, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Operation Director", 100, 4);
+
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Shamim", "Imtiaz", 2);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Chris", "Whiteman", 1);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Ariel", "Hernandez", 3);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Tim", "Squirm", 4);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Eric", "Denosa", 5);
